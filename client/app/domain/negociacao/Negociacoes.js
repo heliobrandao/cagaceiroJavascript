0bb7986c43aef6917c1c1322f9b9ee34226d8c32
heliobrandao <heliob79@gmail.com>
2021-12-05 23:35:25 -0300

    capitulo 6

--- client/app/domain/negociacao/Negociacoes.js
+++ client/app/domain/negociacao/Negociacoes.js
@@ -12,4 +12,22 @@ class Negociacoes {
     return [].concat(this._negociacoes);
 
   }
+
+  get volumeTotal(){
+
+    //IMPLEMENTANDO COM FOR
+    // let total = 0;
+
+    // for(let i = 0; i < this._negociacoes.length; i++){
+    //   total += this._negociacoes[i].volume;
+    // }
+
+    // return total;
+
+    // IMPLEMENTANDO COM reduce()
+
+    return this._negociacoes
+      .reduce((total, negociacao) =>
+        total + negociacao.volume, 0);
+  }
 }
